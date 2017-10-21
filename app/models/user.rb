cc7841bf8a30660c3e651f448dd160ba48773a4d
Eric Peterson <ericmpeterson04@gmail.com>
2017-10-21 17:45:13 -0600

    User sign up completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,25 @@
 class User < ApplicationRecord
+  private
+def confirmation_token
+      if self.confirm_token.blank?
+          self.confirm_token = SecureRandom.urlsafe_base64.to_s
+      end
+    end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+         def email_activate
+             self.email_confirmed = true
+             self.confirm_token = nil
+             save!(:validate => false)
+           end
+           
+         private
+         def confirmation_token
+               if self.confirm_token.blank?
+                   self.confirm_token = SecureRandom.urlsafe_base64.to_s
+               end
+             end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,11 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  resources :users do
+    member do
+      get :confirm_email
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
